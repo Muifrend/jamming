3c6a8ca1a01cb24bd24f0a34d7e0fbb76ffc12ed
Muifrend <andrzejzadaniadomowe1@gmail.com>
2022-05-17 20:37:54 +0200

    working

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,14 +10,36 @@ import {PlayList} from'../PlayList/PlayList.js'
 
 
 class App extends React.Component {
- 
+  constructor(props) {
+    super(props)
+    this.state = {searchResults : [
+      {
+        name: 'Savior',
+        aritst: 'Kendrick Lamar',
+        album: 'Mr. Moral and The Big Steppers',
+        id: 234
+      },
+      {
+        name: 'Funny Thing',
+        aritst: 'Thundercat',
+        album: 'Drunk',
+        id: 235
+      },
+      {
+        name: 'Lost',
+        aritst: 'Frank Ocean',
+        album: 'Blonde',
+        id: 236
+      }
+    ]}
+  }
   render() {
     return <div>
             <h1>Ja<span className="highlight">mmm</span>ing</h1>
             <div className="App">
               <SearchBar />
               <div className="App-playlist">
-                <SearchResults />
+                <SearchResults searchResults={this.state.searchResults}/>
                 <PlayList />
               </div>
             </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -7,8 +7,8 @@ export class Track extends React.Component {
     render() {
         return <div className="Track">
         <div className="Track-information">
-          <h3></h3>
-          <p> | </p>
+          <h3>{}</h3>
+          <p>{} | {}</p>
         </div>
         <button className="Track-action">{this.renderAction}</button>
       </div>
